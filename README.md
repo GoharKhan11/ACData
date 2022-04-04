ae0e4852eecb7636cfaa6587d1bddbebaf99ef94
Gohar Khan <goharayub2008@gmail.com>
2022-04-04 00:56:45 -0400

    modified README, added base subroutine to main script and set up LWP.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # ACData
 
 A program to read data from ashes webpage and store data on desired classes.
+
+Goals:
+- Learn regex usage.
+- Learn perl
+- Learn to use LWP library (basics).
+- Practice using git.
+
+Method:
+- Gets html from target page.
+- Recognize html patterns (manually).
+- extract comments from the comment thread using patterns.
+- Use patterns/key words to find desired class demographic.
--- main.pl
+++ main.pl
@@ -1,9 +1,65 @@
 #!/usr/bin/perl
-use LWP::UserAgent;
-use LWP::Protocol::https;
 
-$UserAgent = new LWP::UserAgent;
-$Request = new HTTP::Request("get", "https://forums.ashesofcreation.com/discussion/46150/what-class-will-you-be-picking#latest");
-$Response = $UserAgent->request($Request);
+use strict;
+use warnings;
 
-print $Response->(_content);
+use LWP::Simple qw($ua get);
+
+# Gets UserAgent
+$ua->agent('My agent/1.0');
+
+# Gets html from url
+my $url = "https://forums.ashesofcreation.com/discussion/46150/what-class-will-you-be-picking#latest";
+my $html = get $url || die "Timed out!";
+
+# Creates a hash to insert author-comment pairs into.
+our %comments;
+
+sub extractComments {
+  # (str, hash) -> null
+  # Takes html file from a page of the target comment thread and extracts
+  # The commentors and associated comments as a key value pair in the
+  # provided hash
+  # ----------------------------
+
+
+  # Makes a list with each line of the html as an element
+  my @lines = split(/\n/, $_[0]);
+  print "Number of lines: ", scalar @lines, "\n";
+
+  # String to match with to find beginning of comment box
+  my $commentBoxMatch = "<ul class=\"MessageList DataList Comments pageBox\">";
+
+  # Finds where comment divider starts
+  our $startCounter = 0;
+  until ($lines[$startCounter] =~ /$commentBoxMatch/) {
+    $startCounter++;
+  }
+
+  # For testing
+  print " The comment thread divider tag is on line: $startCounter\n";
+  print "The comment divider line is: $lines[$startCounter]\n";
+
+  # Finds where comment thread ends on current page
+  # Each comment is essentially an <li> element for a long <ul>
+  our $endCounter = $startCounter;
+  until ($lines[$endCounter] =~/<\/ul>/) {
+    $endCounter++;
+  }
+
+  # For testing
+  print "comment thread ends on line: $endCounter\n";
+  print "comment thread ends on tag: $lines[$endCounter]\n";
+
+  my @thread = splice(@lines, $startCounter, ($endCounter - $startCounter + 1));
+
+  our $threadCounter = 0;
+  foreach my $line (@thread) {
+    print "Line number $threadCounter: $line\n";
+    $threadCounter++;
+  }
+
+# End of subroutine extractComments
+}
+
+extractComments($html);
